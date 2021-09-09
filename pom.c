7ba7f640f7db73c06c0a703155cc6a88f64862c2
Will Pryor <will21@vt.edu>
2021-09-09 15:10:41 -0700

    wrote a lot of the framework for the timer

---
+++ pom.c
@@ -0,0 +1,86 @@
+/**
+* Pomodoro timer in C
+*
+* dev - Will Pryor
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+
+/*    CONSTANTS         */
+#define WORK_TIME 25 // time (min) to work for 1 cycle
+#define REST_TIME 5 // time (min) to rest after each cycle
+#define NUM_CYCLES 4 // number of cycles before a longer break
+#define BREAK_TIME 20 // time (min) to rest after NUM_CYCLES
+
+/*    FUNCTIONS         */
+
+/**
+* Converts seconds (time_t) to minutes (float)
+*/
+float secondsToMin(time_t seconds) {
+  return (float)seconds / 60;
+}
+
+/**
+* Converts seconds (float) to min (time_t)
+*/
+time_t minToSeconds(float min) {
+  return (time_t) (min * 60);
+}
+
+/**
+* Returns the time in seconds since Jan 1 1970
+*/
+time_t getTime() {
+  time_t t;
+  t = time(NULL);
+  return t;
+}
+/**
+* Displays time on the screen
+*     -not sure how yet - options for CLI printout vs gui??
+*     -start with CLI
+*     -display a star per min to indicate progress out of 25
+*/
+void displayTime(int num_stars) { // TODO: make enum status
+  fflush(stdout); // TODO : necessary??
+  system("clear");
+  if (num_stars == WORK_TIME) { // TODO : add audio or something
+    printf("Done working! Take a break for %d minutes!\n", REST_TIME);
+  }
+  else {
+    char * stars = malloc(sizeof(char) * WORK_TIME);
+    int i = 0;
+    for (; i < num_stars; i++) { // TODO : more efficient way??
+      stars[i] = '*';
+    }
+    for (; i < WORK_TIME; i++) {
+      stars[i] = '-';
+    }
+    printf("Progress: %d min out of %d min\n\n", num_stars, WORK_TIME);
+    printf("[ %s ]\n", stars);
+    free(stars);
+  }
+}
+
+
+int main(int argc, char *argv[]) {
+  time_t startTime= getTime(); // starting time
+  time_t currTime = startTime;
+  time_t timePassed = 0;
+  system("clear");
+  displayTime(0);
+  // program loop:
+  while(1) {
+    sleep(10); // TODO : how to not busy wait?? see systems code
+    currTime = time(NULL);
+    timePassed = currTime - startTime;
+    if (timePassed % 60 == 0) {
+      displayTime(secondsToMin(timePassed));
+    }
+  }
+
+  return 0;
+}
